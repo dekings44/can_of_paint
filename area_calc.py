30ea52f2d9f33bc1866efaf19db766ead9063140
Kingsley Ihemere <dekings44@gmail.com>
2022-05-26 22:34:35 +0100

    updated the rounding up with math method

--- area_calc.py
+++ area_calc.py
@@ -1,5 +1,5 @@
 #Calculating the number of cans of paint needed to paint a given wall
-
+import math
 
 height = int(input('What is the height of the wall?\n'))
 width = int(input('What is the width of the wall?\n'))
@@ -8,7 +8,7 @@ coverage = 5
 
 def paint_can_calc(height, width, cover):
     number_of_cans = (height * width) / cover
-    print(f'The number of cans required to paint a wall of {height} and {width} is {round(number_of_cans)}')
+    print(f'The number of cans required to paint a wall of {height} and {width} is {math.ceil(number_of_cans)}')
 
 
 paint_can_calc(height=height, width=width, cover=coverage)
