f64b8768fb0d36f3c58b375be084b41dbb93dcbb
Kingsley Ihemere <dekings44@gmail.com>
2022-05-26 22:32:25 +0100

    first commit

---
+++ area_calc.py
@@ -0,0 +1,14 @@
+#Calculating the number of cans of paint needed to paint a given wall
+
+
+height = int(input('What is the height of the wall?\n'))
+width = int(input('What is the width of the wall?\n'))
+
+coverage = 5
+
+def paint_can_calc(height, width, cover):
+    number_of_cans = (height * width) / cover
+    print(f'The number of cans required to paint a wall of {height} and {width} is {round(number_of_cans)}')
+
+
+paint_can_calc(height=height, width=width, cover=coverage)
